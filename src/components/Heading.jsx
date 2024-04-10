f20d5b57af335955917ad3173b8691b5e9b56084
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-04-10 14:58:25 +0300

    fix header size

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,7 +1,7 @@
 function Heading({ className, title }) {
   return (
     <div className={`${className} max-w-[50rem] max-auto mb-12 lg:mb-20`}>
-      {title && <h2 className="h2">{title}</h2>}
+      {title && <h5 className="h5">{title}</h5>}
     </div>
   );
 }
