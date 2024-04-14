c669cb83008b528198987986f94d9936de03db64
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-04-14 15:15:02 +0300

    add class

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -8,7 +8,7 @@ function Collaboration() {
           <h2 className="h2 mb-4 md:mb-8">
             AI Chat App for seamless collaboration
           </h2>
-          <ul></ul>
+          <ul className="max-w-[22rem] mb-10 md:mb-14"></ul>
         </div>
       </div>
     </Section>
