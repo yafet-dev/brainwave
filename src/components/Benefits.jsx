93a9d6d4a8391c534ee6db5026e5da30e8899345
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-04-20 12:26:20 +0300

    add height

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -25,7 +25,7 @@ function Benefits() {
                 <h5 className="h2 mb-5">{item.title}</h5>
                 <p className="body-2 mb-6 text-n-3">{item.text}</p>
                 <div className="flex items-center">
-                  <img src={item.iconUrl} width={48} />
+                  <img src={item.iconUrl} width={48} height={48} />
                 </div>
               </div>
             </div>
