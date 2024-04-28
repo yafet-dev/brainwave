e3e091ac26ac9a83219b01687510abb46384d91e
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-04-28 15:13:37 +0300

    add mb

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -5,7 +5,9 @@ function Collaboration() {
     <Section crosses>
       <div className="container lg:flex">
         <div className="max-w-[25rem]">
-          <h2 className="h2 mb-4">AI Chat App for seamless collaboration</h2>
+          <h2 className="h2 mb-4 md:mb-8">
+            AI Chat App for seamless collaboration
+          </h2>
         </div>
       </div>
     </Section>
