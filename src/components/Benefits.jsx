d8a0e3b68c54a11df1439f129426cc3ff9edc7b2
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-05-04 12:03:52 +0300

    add benefit section

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,10 +1,16 @@
 import Section from "./Section";
+import Heading from "./Heading";
 
 function Benefits() {
   return (
     <Section id="features">
       <div className="container relative z-2">
-        <Heading />
+        <Heading
+          className="md:max-w-md lg:max-w-2xl"
+          title={"chat smarter not harder"}
+        />
+
+        <div className="flex flex-wrap"></div>
       </div>
     </Section>
   );
