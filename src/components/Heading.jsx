130fdbb94f3565940f8c5c81a5fcfd7fa199636c
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-05-04 12:04:08 +0300

    add heading section

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,6 +1,6 @@
 function Heading({ className, title }) {
   return (
-    <div className={`${className} max`}>
+    <div className={`${className} max-w-[50rem] max-auto mb-12 lg:mb-20`}>
       {title && <h2 className="h2">{title}</h2>}
     </div>
   );
