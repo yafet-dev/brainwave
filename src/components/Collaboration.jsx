7a67262fd8e734fc646b0af335d5c487b09e23a1
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-05-12 16:01:14 +0300

    fix style issue

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -54,7 +54,9 @@ function Collaboration() {
                   className={`absolute top-0 left-1/2 h-1/2 -ml-[1.6rem] origin-bottom 
                     rotate-${index * 45}`}
                 >
-                  <div className={`relative -top-[1.6rem]`}>
+                  <div
+                    className={`relative -top-[1.6rem] flex w-[3.2rem] h-[3.2rem]`}
+                  >
                     <img
                       src={app.icon}
                       className="m-auto"
