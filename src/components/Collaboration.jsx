705b19248aea0ead22f27ea4a256d22e4068813f
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-05-22 15:07:35 +0300

    add collab

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -3,7 +3,9 @@ import Section from "./Section";
 function Collaboration() {
   return (
     <Section crosses>
-      <div className="container lg:flex"></div>
+      <div className="container lg:flex">
+        <div></div>
+      </div>
     </Section>
   );
 }
