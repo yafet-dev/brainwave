479d0785a0b1bf51ad37464cd3b0b485e32a6090
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-06-22 11:58:19 +0300

    add heading component

---
+++ src/components/Heading.jsx
@@ -0,0 +1,9 @@
+function Heading({ className, title }) {
+  return (
+    <div className={`${className}`}>
+      {title && <h2 className="h2">{title}</h2>}
+    </div>
+  );
+}
+
+export default Heading;
