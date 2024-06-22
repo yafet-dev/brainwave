1668f6c70b09bf993774be70d72d778fc856c291
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-06-22 11:53:01 +0300

    update main app route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
 import Header from "./components/Header";
 
 import Hero from "./components/Hero";
@@ -8,6 +9,7 @@ function App() {
       <div className="lg:mt-10 overflow-hidden lg:pt-[5.25rem]">
         <Header />
         <Hero />
+        <Benefits />
       </div>
       <ButtonGradient />
     </>
