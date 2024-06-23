7ac75bf62a37dd79bc6b6357ee44a782de8cfa0b
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-06-23 17:03:27 +0300

    mobile version nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,11 @@ function Header() {
   useState[(openNavigation, setOpenNavigation)] = useState(false);
 
   return (
-    <div className="fixed top-0 left-0 w-full py-6 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
+    <div
+      className={`fixed top-0 left-0 w-full py-6 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+        openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
+      }`}
+    >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
           <img src={brainwave} width={190} height={40} alt="brain wave" />
