203eab1307539c1f1bfc01d310b91e35cc2fe5d3
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-06-25 00:14:29 +0300

    pass probs in the section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,15 @@
 import Section from "./Section";
 
 function Hero() {
-  return <Section>section</Section>;
+  return (
+    <Section
+      className="pt-[12rem] -mt-[5.25]"
+      crosses
+      crossesOffset="lg:translate-y-[5.25rem]"
+    >
+      section
+    </Section>
+  );
 }
 
 export default Hero;
