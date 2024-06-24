ce077fdd30c25a1b1126e7498efa2f5c68fb6466
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-06-24 22:38:41 +0300

    add section component

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,5 +1,25 @@
+import { Children } from "react";
+
 function Section() {
-  return <div>section</div>;
+  const Section = {
+    className,
+    id,
+    crosses,
+    crossesOffset,
+    customPaddings,
+    Children,
+  };
+  return (
+    <div
+      className={`relative ${
+        customPaddings ||
+        `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""} 
+        ${className || ""}`
+      }`}
+    >
+      {Children}
+    </div>
+  );
 }
 
 export default Section;
