56e0afb60d966b16a803c9b964c137f8692e4a10
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-06-28 00:17:47 +0300

    add header to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,8 +6,13 @@ function Hero() {
       className="pt-[12rem] -mt-[5.25]"
       crosses
       crossesOffset="lg:translate-y-[5.25rem]"
+      id="hero"
     >
-      section
+      <div className="container relative">
+        <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]">
+          <h1 className="h1 mb-6"></h1>
+        </div>
+      </div>
     </Section>
   );
 }
