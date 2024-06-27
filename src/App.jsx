d08bb1d373bd712691f423f53da5e21f78fe6507
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-06-27 23:09:16 +0300

    update mail app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from "./components/Header";
-import Button from "./components/Button";
+
+import Hero from "./components/Hero";
 function App() {
   return (
     <>
       <div className="lg:mt-10 overflow-hidden lg:pt-[5.25rem]">
         <Header />
+        <Hero />
       </div>
       <ButtonGradient />
     </>
