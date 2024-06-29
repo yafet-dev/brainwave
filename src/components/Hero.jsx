7685811940bea5bf73a53fce1fc9a7733fb1e8ae
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-06-29 17:01:02 +0300

    add cta button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import Section from "./Section";
 import { curve } from "../assets/index";
-
+import Button from "./Button";
 function Hero() {
   return (
     <Section
@@ -24,6 +24,11 @@ function Hero() {
               />
             </span>
           </h1>
+          <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
+            Unleash the power of AI within Brainwave. Upgrade your productivity
+            with Brainwave, the open AI chat app.
+          </p>
+          <Button>Get started</Button>
         </div>
       </div>
     </Section>
