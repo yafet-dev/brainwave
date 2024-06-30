11ee97815fc329ffc7ff13a3ea5838f74a189844
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-06-30 17:00:03 +0300

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,11 @@ import { useLocation } from "react-router-dom";
 import Button from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
 import { HamburgerMenu } from "./design/Header";
+import { useState } from "react";
 function Header() {
   const pathname = useLocation();
+  useState[(openNavigation, setOpenNavigation)] = useState(false);
+
   return (
     <div className="fixed top-0 left-0 w-full py-6 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
