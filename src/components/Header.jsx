00738f1a3da8f6d6923e5a936a0aceba7f37e5b1
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-08-19 16:31:04 +0300

    special effect on nav bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
 import { brainwave } from "../assets";
 import { navigation } from "../constants/index";
+
+import { useLocation } from "react-router-dom";
 function Header() {
+  const pathname = useLocation();
   return (
     <div className="fixed top-0 py-6 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
@@ -16,7 +19,11 @@ function Header() {
                   href={item.url}
                   className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1  ${
                     item.onlyMobile ? "lg:hidden" : ""
-                  } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold`}
+                  } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
+                    item.url === pathname.hash
+                      ? "z-2 lg:text-n-1"
+                      : "lg:text-n-1/50"
+                  }`}
                 >
                   {item.title}
                 </a>
