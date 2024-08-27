13e80b77b59e0835a4348336d670026efc4c9df7
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-08-27 16:52:39 +0300

    add button gradient

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
 function App() {
-  return <div className="text-5xl text-center ">working</div>;
+  return (
+    <>
+      <div className="text-5xl text-center ">working</div>
+      <ButtonGradient />
+    </>
+  );
 }
 
 export default App;
