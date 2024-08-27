f0795de66e6a52c64dbc14910e6c4768bd565fa5
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-08-27 16:57:55 +0300

    add button gradient

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
 function App() {
   return (
     <>
       <div className="text-5xl text-center ">working</div>
       <ButtonGradient />
+      <div className="pt-[4.75rem] overflow-hidden lg:pt-[5.25rem]">
+        <Button className="mt-10" href="#login">
+          Loggin
+        </Button>
+      </div>
     </>
   );
 }
