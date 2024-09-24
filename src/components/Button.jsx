47785af89fa56eab5b20fc6a1485b475dbefa6dd
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-09-24 14:45:30 +0300

    add styling to the button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,10 @@
 import ButtonSvg from "../assets/svg/ButtonSvg";
 const Button = ({ className, href, onClick, children, px, white }) => {
+  const classes = `button relative inline-flex items-center justify-cneter h-11 transition-colors hover:text-color-1 ${
+    px || "px-7"
+  } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
   const renderButton = () => (
-    <button>
+    <button className={classes}>
       <span>{children}</span>
       {ButtonSvg(white)}
     </button>
