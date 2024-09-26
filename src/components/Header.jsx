d522e76d41ae2f86a55f5860eae500de5789894a
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-09-26 23:42:37 +0300

    fix err

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,7 +23,7 @@ function Header() {
         openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
       }`}
     >
-      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
+      <div className="fle items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
           <img src={brainwave} width={190} height={40} alt="brain wave" />
         </a>
