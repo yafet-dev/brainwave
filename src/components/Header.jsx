2b3a7f9acf771f1dc61a0144cd1b41d3e2f4ee26
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-09-28 22:26:06 +0300

    fix head issue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
-import { brainwave } from "../assets";
-import { navigation } from "../constants/index";
-import { disablePageScroll, enablePageScroll } from "scroll-lock";
 import { useLocation } from "react-router-dom";
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
+
+import { brainwave } from "../assets";
+import { navigation } from "../constants";
 import Button from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
 import { HamburgerMenu } from "./design/Header";
 import { useState } from "react";
-function Header() {
+
+const Header = () => {
   const pathname = useLocation();
   const [openNavigation, setOpenNavigation] = useState(false);
 
@@ -16,24 +18,28 @@ function Header() {
       enablePageScroll();
     } else {
       setOpenNavigation(true);
+      disablePageScroll();
     }
   };
+
   const handleClick = () => {
     if (!openNavigation) return;
+
     enablePageScroll();
     setOpenNavigation(false);
   };
 
   return (
     <div
-      className={`fixed top-0 left-0 w-full py-6 z-50 border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+      className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
         openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
       }`}
     >
-      <div className="fle items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
+      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
-          <img src={brainwave} width={190} height={40} alt="brain wave" />
+          <img src={brainwave} width={190} height={40} alt="Brainwave" />
         </a>
+
         <nav
           className={`${
             openNavigation ? "flex" : "hidden"
@@ -63,18 +69,22 @@ function Header() {
 
         <a
           href="#signup"
-          className="button  hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
+          className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
         >
           New account
         </a>
-
         <Button className="hidden lg:flex">Sign in</Button>
-        <Button className="lg:hidden ml-auto " onClick={toggleNavigation}>
+
+        <Button
+          className="ml-auto lg:hidden"
+          px="px-3"
+          onClick={toggleNavigation}
+        >
           <MenuSvg openNavigation={openNavigation} />
         </Button>
       </div>
     </div>
   );
-}
+};
 
 export default Header;
