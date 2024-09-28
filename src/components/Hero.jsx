939dfc4c4ad329b76458f8303d28eb691026dc02
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-09-29 00:18:44 +0300

    add header

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ function Hero() {
     >
       <div className="container relative">
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]">
-          <h1 className="h1 mb-6"></h1>
+          <h1 className="h1 mb-6">Explore the possibilities</h1>
         </div>
       </div>
     </Section>
