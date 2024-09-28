c56b0f73838c54712c6c6b2bc406efa44872d61a
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-09-29 00:49:00 +0300

    add header

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,9 @@ function Hero() {
     >
       <div className="container relative">
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]">
-          <h1 className="h1 mb-6">Explore the possibilities</h1>
+          <h1 className="h1 mb-6">
+            Explore the possibilities of AI chatting with Brainwave
+          </h1>
         </div>
       </div>
     </Section>
