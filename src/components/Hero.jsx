bd83605bdfcc9c1f5980604b1d01a7e132fad659
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-09-30 16:53:31 +0300

    add some styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,10 +12,16 @@ function Hero() {
       <div className="container relative">
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]">
           <h1 className="h1 mb-6">
-            Explore the possibilities of AI chatting with
+            Explore the possibilities of AI chatting with &nbsp;
             <span className="inline-block relative">
               Brainwave
-              <img src={curve} className="absolute top-full" />
+              <img
+                src={curve}
+                className="absolute top-full left-0 w-full xl:-mt-2"
+                width={624}
+                height={28}
+                alt="curve"
+              />
             </span>
           </h1>
         </div>
