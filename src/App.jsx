000de41cec23f2b5f02ce137a9b7e6e87dc935b8
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-10-01 16:53:58 +0300

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
 import Services from "./components/Services";
 function App() {
   return (
@@ -14,6 +17,9 @@ function App() {
         <Benefits />
         <Collaboration />
         <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
       </div>
       <ButtonGradient />
     </>
--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,7 +1,8 @@
-function Heading({ className, title }) {
+function Heading({ className, title, text }) {
   return (
     <div className={`${className} max-w-[50rem] max-auto mb-12 lg:mb-20`}>
       {title && <h5 className="h5">{title}</h5>}
+      {text && <p className="body-2 mt-4 text-n-4">{text}</p>}
     </div>
   );
 }
