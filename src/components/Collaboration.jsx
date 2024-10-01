66dcdac4020c098b6bfa8cfcaf850d8c50f61091
Yafet Zerihun <zerihunyafet80@gmail.com>
2024-10-01 15:24:23 +0300

    add try it now button

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -1,7 +1,7 @@
 import { check } from "../assets";
 import { collabContent } from "../constants";
 import Section from "./Section";
-
+import Button from "./Button";
 function Collaboration() {
   return (
     <Section crosses>
@@ -17,9 +17,14 @@ function Collaboration() {
                   <img src={check} width={24} height={24} />
                   <h6 className="body-2 ml-5">{item.title}</h6>
                 </div>
+                {item.text && (
+                  <p className="body-2 mt-3 text-n-4">{item.text}</p>
+                )}
               </li>
             ))}
           </ul>
+
+          <Button>Try it now</Button>
         </div>
       </div>
     </Section>
